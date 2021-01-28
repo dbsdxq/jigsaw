35f9266eeebe5fb1f9033434e091d082d5fd667a
xiaomei <844980183@qq.com>
2021-01-28 12:51:28 +0800

    二维数组显示图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,33 +5,36 @@ const app = getApp()
 Page({
   data: {
     pictures: [
-      {
-        fragment_no: 1
-      },
-      {
-        fragment_no: 2
-      },
-      {
-        fragment_no: 3
-      },
-      {
-        fragment_no: 4
-      },
-      {
-        fragment_no: 5
-      },
-      {
-        fragment_no: 6
-      },
-      {
-        fragment_no: 7
-      },
-      {
-        fragment_no: 8
-      },
-      // {
-      //   fragment_no: 9
-      // },
+      [{
+          fragment_no: 1
+        },
+        {
+          fragment_no: 2
+        },
+        {
+          fragment_no: 3
+        },
+      ],
+      [{
+          fragment_no: 4
+        },
+        {
+          fragment_no: 5
+        },
+        {
+          fragment_no: 6
+        },
+      ],
+      [{
+          fragment_no: 7
+        },
+        {
+          fragment_no: 8
+        },
+        // {
+        //   fragment_no: 9
+        // },
+      ]
     ]
   },
 })
