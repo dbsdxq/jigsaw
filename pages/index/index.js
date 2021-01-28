fb1997131238de43ac8bdf10a9c51279e0374353
xiaomei <844980183@qq.com>
2021-01-28 12:38:19 +0800

    显示静态图片分割成九宫格效果

---
+++ pages/index/index.js
@@ -0,0 +1,37 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    pictures: [
+      {
+        fragment_no: 1
+      },
+      {
+        fragment_no: 2
+      },
+      {
+        fragment_no: 3
+      },
+      {
+        fragment_no: 4
+      },
+      {
+        fragment_no: 5
+      },
+      {
+        fragment_no: 6
+      },
+      {
+        fragment_no: 7
+      },
+      {
+        fragment_no: 8
+      },
+      // {
+      //   fragment_no: 9
+      // },
+    ]
+  },
+})
